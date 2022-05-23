1e915e36ce736f0cebe28d0b8e5013341f53b66f
sth4881 <sth4881@naver.com>
2022-05-23 09:08:13 +0900

    [Init] 🎉 JDBC & DataSource 테스트 환경 구축

---
+++ GoGuMa/src/test/java/com/ggm/goguma/mapper/DataSourceTests.java
@@ -0,0 +1,33 @@
+package com.ggm.goguma.mapper;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class DataSourceTests {
+	@Setter(onMethod_ = { @Autowired })
+	private DataSource dataSource;
+	
+	@Test
+	public void testConnection() {
+		try(Connection conn = dataSource.getConnection()) {
+			log.info(conn);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
