97ac7eca59991f6d7e5e2619ba73ade22f006f88
sth4881 <sth4881@naver.com>
2022-06-03 21:19:01 +0900

    [Update] 🔥 PointDTO 내용 업데이트

--- GoGuMa/src/main/java/com/ggm/goguma/dto/PointDTO.java
+++ GoGuMa/src/main/java/com/ggm/goguma/dto/PointDTO.java
@@ -1,5 +1,7 @@
 package com.ggm.goguma.dto;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
@@ -7,5 +9,8 @@ public class PointDTO {
 	private long pointEventId;
 	private String pointType;
 	private long pointValue;
+	private long inquirable;
+	private Date pointCreatedDate;
 	private long receiptId;
+	private long orderId;
 }
